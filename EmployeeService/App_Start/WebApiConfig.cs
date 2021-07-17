73cbddca3f2971d2e2af99fb839685c71cf38005
Vivek Bharad <vhbharad@hotmail.com>
2021-07-17 16:13:53 -0600

    Custom JSON Formatter derived from JsonMediaTypeFormatter

--- EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -3,11 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Net.Http.Headers;
 
 namespace EmployeeService
 {
     public static class WebApiConfig
     {
+        // Another approach for formatting JSON from browser
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+        }
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -29,8 +48,11 @@ namespace EmployeeService
             // 2. How to return only XML formatted data from API
             // config.Formatters.Remove(config.Formatters.JsonFormatter);
 
-            // 3. When request is made from  Web Browser
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            // 3a. When request is made from  Web Browser
+            // config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
+            // 3b. Register our custom Formatter <=> When request is made from  Web Browser
+            config.Formatters.Add(new CustomJsonFormatter());
 
             // Indent JSON data
             // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
